8c702f935b887c9735a681d53e51b2f4f20d11fb
Austin Schneider <hogenshpogen@gmail.com>
2018-07-04 15:31:30 -0500

    Comments. Cleanup. Migrate to numpy built-ins.

--- SAYLikelihood.py
+++ SAYLikelihood.py
@@ -1,76 +1,117 @@
 import numpy as np
 import scipy as sp
 from scipy import special
-
-def accumulate(iterable):
-    sum = 0
-    running_error = 0
-    temp = None
-    difference = None
-
-    for it in iterable:
-        difference = it
-        difference -= running_error
-        temp = sum
-        temp -= difference
-        running_error = temp
-        running_error -= sum
-        running_error -= difference
-        sum = temp
-    return sum
-
-def LogOnePlusX(x):
-    if x <= -1.0:
-        raise ValueError("Invalid input argument(" + str(x) + "); must be greater than -1.0")
-    if abs(x) > 1e-4:
-        return np.log(1.0 + x)
-    return x - np.power(x, 2.0)/2.0 + np.power(x, 3.0)/3.0 - np.power(x, 4.0)/4.0
+from scipy import stats
 
 def gammaPriorPoissonLikelihood(k, alpha, beta):
+    """Poisson distribution marginalized over mean priored
+       with a gamma distribution that has parameters (alpha, beta)
+
+    Parameters
+    ----------
+    k : int
+        The number of observed events
+    alpha : float
+        Gamma distribution shape parameter
+    beta : float
+        Gamma distribution rate parameter
+    """
     values = [
         alpha*np.log(beta),
         sp.special.loggamma(k+alpha).real,
         -sp.special.loggamma(k+1.0).real,
-        -(k+alpha)*LogOnePlusX(beta),
+        -(k+alpha)*np.log1p(beta),
         -sp.special.loggamma(alpha).real,
         ]
-    return accumulate(values)
+    return np.sum(values)
+
+def poissonLikelihood(k, weight_sum, weight_sq_sum):
+    """Computes Log of the Poisson Likelihood
+
+    Parameters
+    ----------
+    k : int
+        the number of observed events
+    weight_sum : float
+        the sum of the weighted MC event counts
+    weight_sq_sum : float
+        the sum of the square of the weighted MC event counts
+
+    Returns
+    -------
+    float
+        The log likelihood
+    """
+
+    return sp.stats.poisson.pmf(k, weight_sum)
 
 def SAYMMSELLH(k, weight_sum, weight_sq_sum):
-    w_sum = weight_sum
-    w2_sum = weight_sq_sum
-    
-    if(w_sum <= 0 or w2_sum < 0):
+    """Computes Log of the SAY Likelihood using Minimum Mean Squared Estimator (MMSE)
+
+    Parameters
+    ----------
+    k : int
+        the number of observed events
+    weight_sum : float
+        the sum of the weighted MC event counts
+    weight_sq_sum : float
+        the sum of the square of the weighted MC event counts
+
+    Returns
+    -------
+    float
+        The log likelihood
+    """
+
+    # Return -inf for ill formed an likelihood or 0 without observation
+    if weight_sum <= 0 or weight_sq_sum < 0:
         if k == 0:
             return 0
         else:
             return -np.inf
 
-    if w2_sum == 0:
-        return poissonLikelihood(k, w_sum, w2_sum)
-    
-    alpha = np.power(w_sum, 2.0) / w2_sum
-    beta = w_sum / w2_sum
+    # Return the poisson likelihood in the appropriate limiting case
+    if weight_sq_sum == 0:
+        return poissonLikelihood(k, weight_sum, weight_sq_sum)
+
+    alpha = np.power(weight_sum, 2.0) / weight_sq_sum
+    beta = weight_sum / weight_sq_sum
     L = gammaPriorPoissonLikelihood(k, alpha, beta)
     return L
 
 def SAYMAPLLH(k, weight_sum, weight_sq_sum):
-    w_sum = weight_sum
-    w2_sum = weight_sq_sum
-    
-    if(w_sum <= 0 or w2_sum < 0):
+    """Computes Log of the SAY Likelihood using Maximum A'postiori Probability (MAP) estimator
+
+    Parameters
+    ----------
+    k : int
+        the number of observed events
+    weight_sum : float
+        the sum of the weighted MC event counts
+    weight_sq_sum : float
+        the sum of the square of the weighted MC event counts
+
+    Returns
+    -------
+    float
+        The log likelihood
+    """
+
+    # Return -inf for ill formed an likelihood or 0 without observation
+    if weight_sum <= 0 or weight_sq_sum < 0:
         if k == 0:
             return 0
         else:
             return -np.inf
 
-    if w2_sum == 0:
-        return poissonLikelihood(k, w_sum, w2_sum)
-    
-    mu = w_sum
+    # Return the poisson likelihood in the appropriate limiting case
+    if weight_sq_sum == 0:
+        return poissonLikelihood(k, weight_sum, weight_sq_sum)
+
+    mu = weight_sum
     mu2 = np.power(mu, 2.0)
 
-    sigma2 = w2_sum
+    sigma2 = weight_sq_sum
 
     beta = (mu + np.sqrt(mu2 + sigma2*4.0))/(sigma2*2.0)
     alpha = (mu*np.sqrt(mu2 + sigma2*4.0)/sigma2 + mu2/sigma2 + 2.0)/2.0
